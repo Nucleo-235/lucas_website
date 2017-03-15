dcc56f3532ce31ccd6be6a4c6dc0abc87394260b
Henrique Rangel <hrangel@nucleo235.com.br>
2017-03-14 21:08:18 -0300

    adicionado bg como video

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -84,6 +84,25 @@ $(document).ready(function() {
       currentStrategy.openProjectWithLink(this, null, window.location.hash);
     }
   }
+
+  var vid = document.getElementById("bgvid");
+
+  if (window.matchMedia('(prefers-reduced-motion)').matches) {
+      vid.removeAttribute("autoplay");
+      vid.pause();
+  }
+
+  function vidFade() {
+    vid.classList.add("stopfade");
+  }
+
+  vid.addEventListener('ended', function()
+  {
+    // only functional if "loop" is removed 
+    vid.pause();
+    // to capture IE10
+    vidFade();
+  });
 });
 
 function preventDefaultIfPossible(e) {
